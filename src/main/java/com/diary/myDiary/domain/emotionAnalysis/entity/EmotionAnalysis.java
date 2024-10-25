6536e83c6eb195ed9a0f22f526b96127e65d6c8b
HyeonSeongIM <gus8131@naver.com>
2024-10-25 12:29:14 +0900

    Entity 코드 생성 - ERD 설계에 맞게 작성 - 모두 단방향 매핑으로 설계
    주석 위치 수정
    - 각 세부 코드 위에 주석 옮겼습니다.

--- src/main/java/com/diary/myDiary/domain/emotionAnalysis/entity/EmotionAnalysis.java
+++ src/main/java/com/diary/myDiary/domain/emotionAnalysis/entity/EmotionAnalysis.java
@@ -1,14 +1,10 @@
 package com.diary.myDiary.domain.emotionAnalysis.entity;
 
+import com.diary.myDiary.domain.diary.entity.Diary;
+import com.diary.myDiary.domain.member.entity.Member;
 import com.diary.myDiary.global.util.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
@@ -17,10 +13,18 @@ import lombok.NoArgsConstructor;
 @Builder
 public class EmotionAnalysis extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 감정분석 ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emotion_id")
     private Long id;
 
-//    private Diary diary;
+    // diary_id  1:1 관계 매핑
+    @OneToOne
+    @JoinColumn(name = "diary_id", nullable = false)
+    private Diary diary; // Diary 작성자
 
-    private String analysisResult;
+    // emotion_result
+    @Column(name = "emotion_result", nullable = false)
+    private String analysisResult; // emotion_result
 }

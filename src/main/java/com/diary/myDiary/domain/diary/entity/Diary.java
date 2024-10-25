c5299b78020cdfbfdd11f26a9015f90d131e499e
HyeonSeongIM <gus8131@naver.com>
2024-10-25 12:29:14 +0900

    Entity 코드 생성 - ERD 설계에 맞게 작성 - 모두 단방향 매핑으로 설계
    주석 위치 수정
    - 각 세부 코드 위에 주석 옮겼습니다.

--- src/main/java/com/diary/myDiary/domain/diary/entity/Diary.java
+++ src/main/java/com/diary/myDiary/domain/diary/entity/Diary.java
@@ -1,10 +1,13 @@
 package com.diary.myDiary.domain.diary.entity;
 
+import com.diary.myDiary.domain.emotionAnalysis.entity.EmotionAnalysis;
+import com.diary.myDiary.domain.imageGeneration.entity.ImageGeneration;
 import com.diary.myDiary.domain.member.entity.Member;
 import com.diary.myDiary.global.util.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.awt.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -12,21 +15,35 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Table(name = "DIARY")
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class Diary extends BaseTimeEntity {
 
+    // 일기 ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "diary_id")
     private Long id;
 
+    // Diary 작성자
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
-    private Member member; // Diary 작성자
+    @ToString.Exclude
+    private Member member;
 
+    // 일기 내용
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String content; // 일기 내용
+    private String content;
 
+    // 감정 태그
     @Column(name = "emotion_tag", length = 50)
-    private String emotionTag; // 감정 태그
+    private String emotionTag;
 
+    // 이미지 생성과 1:1 매핑
+    @OneToOne(mappedBy = "diary", cascade = CascadeType.ALL)
+    private ImageGeneration imageGeneration;
+
+    // 감정 분석과 1:1 매핑
+    @OneToOne(mappedBy = "diary", cascade = CascadeType.ALL)
+    private EmotionAnalysis emotionAnalysis;
 }

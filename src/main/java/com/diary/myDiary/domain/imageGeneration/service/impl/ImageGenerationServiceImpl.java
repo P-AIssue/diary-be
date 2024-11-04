7d687a41e3fe221143c437cfb30de3c0b953ac54
sonsumin <sonsumin00@gmail.com>
2024-11-04 17:43:06 +0900

    [#11]Feat: ImageGeneration 통합

--- src/main/java/com/diary/myDiary/domain/imageGeneration/service/impl/ImageGenerationServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.diary.myDiary.domain.imageGeneration.service.impl;
-
-import com.diary.myDiary.domain.imageGeneration.service.ImageGenerationService;
-
-public class ImageGenerationServiceImpl {
-
-
-}

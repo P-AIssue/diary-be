481a84f99f8f4b6085313d7c6f6e6643f8be037d
HyeonSeong <gus8131@naver.com>
2024-12-02 00:48:48 +0900

    [#20] 관리자 기능

---
+++ src/main/java/com/diary/myDiary/domain/admin/controller/AdminController.java
@@ -0,0 +1,4 @@
+package com.diary.myDiary.domain.admin.controller;
+
+public class AdminController {
+}
---
+++ src/main/java/com/diary/myDiary/domain/admin/repository/AdminRepository.java
@@ -0,0 +1,4 @@
+package com.diary.myDiary.domain.admin.repository;
+
+public class AdminRepository {
+}

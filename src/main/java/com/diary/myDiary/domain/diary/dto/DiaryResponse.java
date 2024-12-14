41602f8a8341e6c6cba4ba4803c746a9a627757b
sonsumin <sonsumin00@gmail.com>
2024-12-14 21:13:59 +0900

    [#7]Refactor: 조회할 때 dateTime도 같이 가져오기

--- src/main/java/com/diary/myDiary/domain/diary/dto/DiaryResponse.java
+++ src/main/java/com/diary/myDiary/domain/diary/dto/DiaryResponse.java
@@ -2,6 +2,7 @@ package com.diary.myDiary.domain.diary.dto;
 
 import com.diary.myDiary.domain.diary.entity.Diary;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,19 +10,16 @@ public record DiaryResponse(
         Long id,
         String content,
         String emotionTag,
-        String imageUrl
+        String imageUrl,
+        LocalDate createDate
 ) {
     public static DiaryResponse of(Diary diary) {
-        return new DiaryResponse(diary.getId(), diary.getContent(), diary.getEmotionTag(), diary.getImageUrl());
+        return new DiaryResponse(diary.getId(), diary.getContent(), diary.getEmotionTag(), diary.getImageUrl(), diary.getCreatedDate());
     }
 
     public static List<DiaryResponse> listOf(List<Diary> diaries) {
         return diaries.stream()
                 .map(DiaryResponse::of)
                 .collect(Collectors.toList());
-    }
-
-    public DiaryResponse(String content) {
-        this(null, content, null, null);
     }
 }
